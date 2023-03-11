2cad1a29e85b75e49d9300a03517b3af2103e2c2
Coinstamp <beser.kivanc@gmail.com>
2023-03-12 01:39:40 +0300

    Create IntegerToRoman.java

---
+++ IntegerToRoman.java
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/integer-to-roman/
+
+class Solution {
+    public String intToRoman(int num) {
+        String[] thousands=  new String[]{"", "M", "MM", "MMM"};
+        String[] hundreds=  new String[]{"", "C", "CC", "CCC", "CD", "D","DC","DCC","DCCC","CM"};
+        String[] tens=  new String[]{"", "X", "XX", "XXX", "XL", "L","LX","LXX","LXXX","XC"};
+        String[] units=  new String[]{"", "I", "II", "III", "IV", "V","VI","VII","VIII","IX"};
+        return thousands[num / 1000] +
+        hundreds[(num % 1000) / 100] +
+        tens[(num %100) /10] +
+        units[num %10];
+    }
+}
