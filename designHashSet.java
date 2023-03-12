6e282f15e8400ce31e96abc05dcd80de413c626f
Coinstamp <beser.kivanc@gmail.com>
2023-03-12 14:18:48 +0300

    Create designHashSet.java

---
+++ designHashSet.java
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/design-hashset
+
+class MyHashSet {
+    boolean[] setArray;
+    public MyHashSet() {
+        setArray = new boolean[1000000 + 1];
+    }
+
+    public void add(int key) {
+        setArray[key] = true;
+    }
+
+    public void remove(int key) {
+        setArray[key] = false;
+    }
+
+    public boolean contains(int key) {
+        return setArray[key];
+    }
+}
+
+// TC: O(1)
+// SC: O(10^6 + 1) => O(1)
